121b6592bb6d68d4550cc25cdc52c79ec0a53873
ACE1222333 <acemaratas59@gmail.com>
2025-04-26 20:21:17 +0800

    testing

--- problem100.java
+++ problem100.java
@@ -1,31 +1,12 @@
 import java.util.Scanner;
 
 public class problem100 {
-        public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
-        System.out.print("Enter size: ");
-        int size = scanner.nextInt();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Pogi si Ace");
 
-        int[] arr = new int[size];
-
-        for(int i = 0; i < size; i++) {
-            System.out.print("Enter number on index " + (i + 1) + ": ");
-            int input = scanner.nextInt();
-
-            arr[i] = input;
         }
-
-
-        int max_value = arr[0];
-
-        for(int n : arr) {
-            if (n > max_value) {
-                max_value = n;
-            }
-        }
-
-        System.out.print("Max Value: " + max_value);
         System.out.println("Testing");
 
     }
