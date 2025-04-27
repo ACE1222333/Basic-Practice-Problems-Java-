566c61163a7dc5c80a1a581096cab1e51afa45cd
ACE1222333 <acemaratas59@gmail.com>
2025-04-27 20:02:11 +0800

    Add problem 28.java

---
+++ problem28.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class problem28 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        for(int i = 0; i < size; i++) {
+            System.out.print("Enter a number for index " + i + ": ");
+            int input = scanner.nextInt();
+            arr[i] = input;
+        }
+        
+        int min_value = arr[0];
+        int min_value_2 = Integer.MAX_VALUE;
+
+        for(int number : arr) {
+            if (number < min_value) {
+                int temp = min_value;
+                min_value = number;
+                min_value_2 = temp;
+            } else if (number < min_value_2 && min_value != number) {
+                min_value_2 = number;
+            }
+        }
+
+        if (min_value_2 == Integer.MAX_VALUE) {
+            System.out.println("No second smallest");
+        } else {
+            System.out.print("Smallest number: " + min_value + "\n" + "Second Smallest number: " + min_value_2);
+        }
+         
+    }
+}
