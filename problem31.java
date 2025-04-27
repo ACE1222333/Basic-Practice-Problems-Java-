2736dc062a7cbefee145fa6f57d6c2a60301bb17
ACE1222333 <acemaratas59@gmail.com>
2025-04-27 21:04:40 +0800

    Add problem31.java

---
+++ problem31.java
@@ -0,0 +1,44 @@
+
+
+//Check if an array is a palindrome.
+
+import java.util.Scanner;
+
+public class problem31 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int size = scanner.nextInt();
+        
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter number for index " + i + ": ");
+            int input = scanner.nextInt();
+            arr[i] = input;
+        }
+
+
+        int j = size - 1;
+        boolean isPalindrome = true;
+
+        for (int number : arr) {
+            if (number != arr[j--]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Array is palindrome");
+        } else {
+            System.out.println("Array is not palindrome");
+        }
+
+        scanner.close();
+
+
+
+        
+    }
+}
