8ad35c5d61a985947776563f49832d03ca01e470
ACE1222333 <acemaratas59@gmail.com>
2025-04-28 20:04:54 +0800

    Add File

---
+++ problem34.java
@@ -0,0 +1,27 @@
+
+//Pass an array to a function and print it.
+
+import java.util.Random;
+
+public class problem34 {
+
+    public static void print(int[] arr) {
+        System.out.println("Numbers in the array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random() * 31);
+        }
+
+        print(arr);
+
+
+
+    }
+}
